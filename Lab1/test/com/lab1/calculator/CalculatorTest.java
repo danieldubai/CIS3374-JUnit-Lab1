bdfaeeeca749309012611638ba53e53dfb279745
tuf49198 <tuf49198@cis-se20631.tu.temple.edu>
2017-03-07 17:55:14 -0500

    CIS 3374	Lab 6
    One drawback I found was that double/decimal numbers could not be tested
    becuase the program only initialised integer variables.

---
+++ Lab1/test/com/lab1/calculator/CalculatorTest.java
@@ -0,0 +1,39 @@
+package com.lab1.calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	
+	private Calculator calculator;
+	
+	@Before
+	public void setUp(){
+		calculator = new Calculator();
+	}
+	
+	@Test
+	public void addtest() {
+		assertEquals(4, calculator.add(2, 2));
+	}
+	
+	@Test
+	public void subtracttest() {
+		assertEquals(0, calculator.subtract(2, 2));
+	}
+	
+	@Test
+	public void multiplytest() {
+		assertEquals(8, calculator.multiply(2, 4));
+	}
+	
+	@Test
+	public void dividetest() {
+		assertEquals(3, calculator.divide(6, 2));
+	}
+
+}
